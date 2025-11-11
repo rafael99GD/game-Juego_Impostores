c09ae2b3656fa521f63993c45fb999a8470ba44d
Juan Pérez <rafaelsanchezfernandez12@gmail.com>
2025-11-11 17:32:52 +0100

    cambio URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Juego_Impostores/',
+  base: '/game-Juego_Impostores/',
   publicDir: 'public'
 })
